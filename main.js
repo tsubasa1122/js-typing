c40b32d68f4f064897806885c3d35c10f2b3aa9c
tsubasa1122 <nakanot@open8.com>
2019-10-13 00:40:37 +0900

    keyが合っていたら正解を出すように

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $(function() {
+  let char_index = 1;
+
   $(document).on('keypress', function(e) {
-    alert('key:'+ e.key);
+
+    const $target = $('#char-'+ char_index);
+    const char = $target.text();
+    if (e.key == char) {
+      $target.removeClass('default');
+      $target.addClass('correct');
+      char_index++;
+    }
+
   });
 });
